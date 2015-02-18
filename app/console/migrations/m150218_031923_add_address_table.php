1aeec741e36aad940a59a174ee838214af95bb73
Damien Buttler <damien@doublehops.com>
2015-02-18 14:38:46 +1100

    Added constraint to address -> user relationship.

--- app/console/migrations/m150218_031923_add_address_table.php
+++ app/console/migrations/m150218_031923_add_address_table.php
@@ -8,8 +8,8 @@ class m150218_031923_add_address_table extends Migration
     public function up()
     {
         $this->createTable('address', [
-                'id' => 'INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "Index"',
-                'user_id' => 'INT(10) UNSIGNED NOT NULL',
+                'id' => 'INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "Index"',
+                'user_id' => 'INT(11) NOT NULL',
                 'street_address' => 'VARCHAR(100)',
                 'city' => 'VARCHAR(100)',
                 'country' => 'VARCHAR(100)',
@@ -18,6 +18,8 @@ class m150218_031923_add_address_table extends Migration
                 'updated_at' => 'INT(11)',
             ],  'ENGINE=InnoDB CHARSET utf8 COMMENT "Table of markets"'
         );
+
+        $this->addForeignKey('fk_user_address', 'address', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
